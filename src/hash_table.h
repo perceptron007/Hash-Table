16299642ed56514a5ca31e4ff167d4909515ed44
perceptron007 <dprophecyguy@gmail.com>
2019-01-22 18:20:47 +0530

    Create HT insert function

--- src/hash_table.h
+++ src/hash_table.h
@@ -9,3 +9,9 @@ typedef struct {
   int size;
   ht_item** items;
 } ht_hash_table;
+
+// API's exposed by Hash Table
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+void ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* ht, const char* key);
+
